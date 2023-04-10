c34ded70a3eab55d584fda37aa7a03379c71f6f6
Vladimir Sivakov <vladimir.a.sivakov@gmail.com>
2023-04-10 23:24:55 +0300

    appveyor and husky added

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 export default function checkHealth(character) {
   if (character.health > 50) {
     return 'healthy';
-  } else if (character.health > 15) {
-    return 'wounded';
-  } else {
-    return 'critical';
   }
+  if (character.health > 15) {
+    return 'wounded';
+  }
+  return 'critical';
 }
